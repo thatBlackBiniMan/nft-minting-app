f4891580117dc6d7241e6f7c86c855d83e66b385
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-19 10:22:25 +0100

    Finished Smart Contract part, working on front-end next

--- contracts/GuePunksNFT.sol
+++ contracts/GuePunksNFT.sol
@@ -25,7 +25,35 @@ contract GuePunksNFT is ERC721, Ownable {
   }
 
 function setIsPublicMintEnabled (bool isPublicMintEnabled_) external onlyOwner{
-  
+  isPublicMintEnabled = isPublicMintEnabled_;
 }
+
+function setBaseTokenUri(string calldata baseTokenUri_) external onlyOwner{
+  baseTokenUri = baseTokenUri_;
+}
+
+function tokenURI(uint256 tokenId_) public view override returns (string memory){
+  require (_exists(tokenId_), 'Token does not exist!');
+  return string(abi.encodePacked(baseTokenUri, Strings.toString(tokenId_), ".json"));
+}
+
+function withdraw() external onlyOwner {
+  (bool success, )= withdrawWallet.call{ value: address(this).balance}('');
+   require(success, 'withdraw failed');
+}
+
+//Mint function
+function mint(uint256 quantity_) public payable{
+  require(isPublicMintEnabled, 'minting not enabled');
+  require(msg.value == quantity_ * mintPrice, 'wrong mint value');
+  require(totalSupply + quantity_ <= maxSupply, 'Sold out');
+  require(walletMints[msg.sender] + quantity_ <= maxPerWallet, 'exceed max wallet limit');
+
+  for(uint256 i = 0; i < quantity_; i++){
+    uint256 newTokenId= totalSupply + 1;
+    totalSupply++;
+    _safeMint(msg.sender, newTokenId);
+   }
+ }
 
 }
