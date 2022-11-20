b1619ea501e9442c1964b906538e612a99c9257d
@thatBlackBiniMan <solomonomoruyi66@gmail.com>
2022-11-20 22:35:40 +0100

    Finished app basic structures with alot of debugging to do

--- src/MainMint.js
+++ src/MainMint.js
@@ -21,7 +21,9 @@ async function handleMint (){
       signer
     );
   try {
-   const response = await contract.mint(BigNumber.from (mintAmount));
+   const response = await contract.mint(BigNumber.from (mintAmount), {
+    value: ethers.utils.parseEther((0.02 * mintAmount).toString()),
+   });
    console.log('response');
   } catch(err){
     console.log("error", err)
@@ -46,18 +48,60 @@ return(
     <Box width="520px">
     <div>
     <Text fontSize="48px" textShadow="0 5px #00000">GuePunks</Text>
-    <p>Welcome to Tomorrow, I already exist tomorrow, come explore</p>
+    <Text fontSize="30px" letterSpacing="-5.5%" fontFamily="VT323" textShadow="02px 2px #000000">Welcome to Tomorrow, I already exist tomorrow, come explore</Text>
     {isConnected ? (
       <div>
-        <div>
-        <button onClick={handleDecrement}>-</button>
-        <input type="number" value={mintAmount}/>
-        <button onClick={handleIncrement}>+</button>
-        </div>
-        <button onClick={handleMint}>Mint Now</button>
+        <Flex align="center" justify="center">
+        <Button
+        backgroundColor="#D6517D"
+        borderRadius="5px"
+        boxShadow ="0px 2px 2px 1px #0F0F0F"
+        color="white"
+        cursor="pointer"
+        fontFamily="inherit"
+        padding="15px"
+        marginTop="10px"
+        onClick={handleDecrement}>-</Button>
+        <Input 
+        readOnly
+        
+        fontFamily="inherit"
+        width="100px"
+        height="40px"
+        textAlign="center"
+        paddingLeft="19px"
+        marginTop="10px"
+        type="number" value={mintAmount}/>
+        <Button backgroundColor="#D6517D"
+        borderRadius="5px"
+        boxShadow ="0px 2px 2px 1px #0F0F0F"
+        color="white"
+        cursor="pointer"
+        fontFamily="inherit"
+        padding="15px"
+        marginTop="10px"
+        onClick={handleIncrement}>+</Button>
+        </Flex>
+        <Button 
+        backgroundColor="#D6517D"
+        borderRadius="5px"
+        boxShadow ="0px 2px 2px 1px #0F0F0F"
+        color="white"
+        cursor="pointer"
+        fontFamily="inherit"
+        padding="15px"
+        marginTop="10px"
+        onClick={handleMint}>Mint Now</Button>
         </div>
     ): (
-      <p>Must Connect Wallet to Mint.</p>
+      <Text 
+      marginTop="70px"
+      fontSize="30px" 
+      letterSpacing="-5.5%"
+      fontFamily="VT323" 
+      textShadow="02px 2px #000000"
+      color="#D6517D"
+      >Must Connect Wallet to Mint.</Text>
     )}
  </div>
     </Box>    
